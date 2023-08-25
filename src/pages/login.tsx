c3e04e628cb4372e429906d58b04c4904efe9e45
Salman Ansari <Sp19bscs0038@maju.edu.pk>
2023-08-25 17:23:29 +0500

    Login by react-hook-form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  firstName: string;
+  password: string;
+  email: string;
+};
+
 export const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const api_Url = "http://localhost:3000/";
+  const { reset, register, handleSubmit } = useForm<FormValues>();
+
+  async function fetchData(url: string, param: FormValues) {
+    try {
+      const requestBody = JSON.stringify({ param });
+
+      const response = await fetch(url, {
+        method: "POST", // Use POST method for sending JSON data
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: requestBody,
+      });
+
+      if (!response) {
+        throw new Error(`HTTP error! Status: ${response}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      throw new Error(`Fetch error.`);
+    }
+  }
+
+  const setterFunc = (data: any) => {
+    setEmail(data);
+    reset();
+    // console.log(data);
+    fetchData(api_Url, data);
+  };
 
   return (
     <div
@@ -12,22 +54,45 @@ export const Login = () => {
         alignItems: "center",
       }}
     >
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <h3> Login</h3>
-        <input type="text" placeholder="Enter email" />
-        <input type="text" placeholder="password" />
-        <br />
-        <button
-          onClick={() => {
-            navigate("/account");
+      <div>
+        <form
+          onSubmit={handleSubmit(setterFunc)}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyItems: "end",
           }}
         >
-          Enter
-        </button>
-        <p>
-          Dont have account <a href="/signUp"> Click here</a>{" "}
-        </p>
+          <label>
+            Email:
+            <input type="email" {...register("email")} />
+          </label>
+          <br />
+
+          <label>
+            Password:
+            <input type="password" {...register("password")} />
+          </label>
+
+          <br />
+          <input type="submit" value="submit" />
+        </form>
       </div>
     </div>
   );
 };
+
+// <h3> Login</h3>
+//         <input
+//           type="text"
+//           onChange={(e) => {
+//             handleChange(e);
+//           }}
+//           placeholder="Enter email"
+//         />
+//         <input type="text" placeholder="password" required />
+
+//         <button onClick={() => fetchData(api_Url, email)}>Enter</button>
+//         <p>
+//           Dont have account <a href="/signUp"> Click here</a>{" "}
+//         </p>
